7d2ef8a3b47d9cb78d20c1f512f4c34c0b57f675
MyLV <mydonghung@gmail.com>
2021-05-09 14:08:11 +0700

    ex3 adding agree conditions button handling

--- FrontEndFinalExam/Report/bai3/js/main.js
+++ FrontEndFinalExam/Report/bai3/js/main.js
@@ -12,10 +12,14 @@ function AccountRegisterHandle(){
     let email_val           = document.getElementById("email_address").value;
     let Password_val        = document.getElementById("Password").value;
     let Repeat_Password_val = document.getElementById("Repeat_Password").value;
+    let confirm_conditions  = document.getElementById("confirm_conditions").checked;
 
     if((email_val === "") || (Password_val === "") || (Repeat_Password_val === ""))
     {
-        alert("please check again, required filed is empty");
+        alert("Please check again, required filed is empty!");
+    }
+    else if(confirm_conditions != true){
+        alert("You must to agree iterms and condition above!")
     }
     else{
         // do nothing
