395a19d9afe37e7ff2a97b8930614564bf5d9afe
MyLV <mydonghung@gmail.com>
2021-05-12 00:08:06 +0700

    upload example

---
+++ JavaPractice/hello/Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	public static void feed( Reptile reptile){
+		System.out.println("Feeding reptile " + reptile.getName());
+	}
+	
+	public static void main(String[] args){
+		feed(new Alligator());
+		feed(new Crocodile());
+		feed(new Reptile());
+	}
+}
